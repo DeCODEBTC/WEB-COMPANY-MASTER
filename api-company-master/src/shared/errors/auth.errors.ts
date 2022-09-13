6893dba09d47af4549e99b75795e2d15c052bfc5
DeCODEBTC <crossslove2003@gmail.com>
2022-09-12 22:34:43 -0300

    Tela de registro funcionando

--- api-company-master/src/shared/errors/auth.errors.ts
+++
@@ -1,6 +0,0 @@
-export const AuthErrors = {
-  EmailNotFound: 'Email address or password provided is incorrect.',
-  EmailNotVerifyed: 'Email not verifyed',
-  InvalidPassword: 'Email address or password provided is incorrect.',
-  InvalidToken: 'InvalidToken',
-};
--- api-company-master/src/shared/errors/common.errors.ts
+++
@@ -1,3 +0,0 @@
-export const CommonErrors = {
-  NotFound: 'NotFound',
-};
